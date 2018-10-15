a93841f537329384a4d09f9894200c8aaa57509b
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-10-15 20:28:39 -0300

    Trabajando tansporte a chacao

--- transbordo.c
+++ transbordo.c
@@ -1,21 +1,29 @@
 #include <nSystem.h>
 #include "transbordo.h"
-
+#include "fifoqueues.h"
 
 
 
 
 int[] enPargua;
 int[] enChacao;
-int enViaje;
+int haciaPargua;
+int haciaChacao;
 int numTRansbordadores;
 nMonitor mon;
+FifoQueue esperaHaciaPargua;
+FifoQueue esperaHaciaChacao;
+nCondition transEnChacao;
+nCondition transEnPargua;
 
 void inicializar(int p){
     mon = nMakeMonitor();
+    esperaHaciaChacao = MakeFifoQueue();
+    esperaHaciaPargua = MakeFifoQueue();
     enPargua = new int[p];
     enChacao = new int[p];
-    enViaje = 0;
+    haciaChacao = 0;
+    haciaPargua = 0;
     numTRansbordadores = p;
     for (i=0; i<numTRansbordadores; i++) {
         enPargua[i] = 1;
@@ -23,5 +31,38 @@ void inicializar(int p){
   }
 }
 void finalizar(){}
-void transbordoAChacao(int v){}
+void transbordoAChacao(int v){
+    nEnter(mon);
+    int transdis = -1;
+    while (transdis == -1){
+        if (EmptyFifoQueue(esperaHaciaChacao)){
+            for (int i = 0; i < numTRansbordadores ; ++i) {
+                if (enPargua[i] == 1){
+                    transdis = i;
+                    break;
+                }
+            }
+            if (transdis == -1){
+                if (haciaPargua == 0){
+                    // casos de trans en Chacao
+                }
+                nWait(mon);
+            }
+        } else{
+            PutObj(esperaHaciaChacao, v);
+            nNotifyAll(mon);
+            nWait(mon);
+        }
+
+    }
+    enPargua[transdis] = 0;
+    haciaChacao++;
+    nExit(mon);
+    haciaChacao(transdis, v);
+    nEnter(mon);
+    haciaChacao--;
+    enChacao[transdis] = 1;
+    nNotifyAll(mon);
+    nExit(mon);
+}
 void transbordoAPargua(int v){}
